cc5c431433caadc10d791c38e6a708325f6c9c48
maicolLlanos <maicolredimido10@gmail.com>
2021-06-11 19:56:16 -0400

    se creo el modelo

---
+++ Models/Telas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiTelas.Models
+{
+    public class Telas
+    {
+        [Key]
+        public int id { get; set; }
+        public string marca { get; set; }
+
+        public string ancho { get; set; }
+
+        public string tipoDeTela { get; set; }
+
+        public string color { get; set; }
+        public string pais { get; set; }
+
+        public bool estado { get; set; }
+
+    }
+}
