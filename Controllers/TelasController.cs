db662fb14ecbd6193762c57171e812825cc4830f
maicolLlanos <maicolredimido10@gmail.com>
2021-06-11 20:50:46 -0400

    se creo la bd y se conecto a la bd y se mostro los datos en pantalla

--- Controllers/TelasController.cs
+++ Controllers/TelasController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiTelas.Context;
 
 namespace ApiTelas.Controllers
 {
@@ -11,5 +12,17 @@ namespace ApiTelas.Controllers
     [ApiController]
     public class TelasController : ControllerBase
     {
+        private readonly AppDbContext context;
+        public TelasController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+
+            return Ok(context.telas.ToList());
+        }
+
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
+using ApiTelas.Context;
 namespace ApiTelas
 {
     public class Startup
@@ -26,6 +27,8 @@ namespace ApiTelas
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("sqlServer")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
